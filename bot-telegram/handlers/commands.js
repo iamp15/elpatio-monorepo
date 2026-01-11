5def284f9423248018fd9055c34e20abbf1f2f2a
Igor <iamp15@hotmail.com>
2026-01-11 12:14:50 -0400

    Eliminar bot-telegram del repositorio principal para convertirlo en submodule

--- bot-telegram/handlers/commands.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// Importar la nueva estructura modular de comandos
-const commandsModule = require("./commands/index");
-
-// Re-exportar todas las funciones para mantener compatibilidad
-module.exports = commandsModule;
--- bot-telegram/handlers/messages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// Importar la nueva estructura modular de mensajes
-const messagesModule = require("./messages/index");
-
-// Re-exportar todas las funciones para mantener compatibilidad
-module.exports = messagesModule;
